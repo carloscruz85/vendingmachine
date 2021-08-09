9e7808e1ddf78774e1977e0902f34028339634f0
carloscruz85 <carloscruz85@gmail.com>
2021-08-09 14:29:50 -0600

    Title / top machine created

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,41 +1,74 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import Modal from '../../components/modal';
+import './index.scss';
 
 const Home = () => {
     //control modal
     const [modal, setModal] = useState(
         {
-            show: true,
+            show: false,
             msg: 'Loading...'
         }
     )
 
-    // //getting data from API https://vending-machine-test.vercel.app/api/products
-    // useEffect(() => {
-    //     //display modal
-    //     setModal({
-    //         show: true,
-    //         msg: 'Loading...'
-    //     })
-    //     axios.get('https://vending-machine-test.vercel.app/api/products')
-    //     .then(res => {
-    //         console.log(res);
-    //         //hide modal
-    //         setModal({
-    //             show: false,
-    //             msg: ''
-    //         })
-    //     })
-    //     .catch(error => {
-    //       console.log('Error', error)
-    //     })
-    // }, [])
+    //control data
+    const [data, setData] = useState(
+        [
+            {
+                "id": "-1",
+                "name": "Loading",
+                "preparation_time": 1,
+                "thumbnail": ""
+            }
+        ]
+    )
+
+    //getting data from API https://vending-machine-test.vercel.app/api/products
+    useEffect(() => {
+        //display modal
+        setModal({
+            show: true,
+            msg: 'Loading...'
+        })
+        axios.get('https://vending-machine-test.vercel.app/api/products')
+        .then(res => {
+            // console.log(res);
+            setData(res.data)
+            //hide modal
+            setModal({
+                show: false,
+                msg: ''
+            })
+        })
+        .catch(error => {
+          console.log('Error', error)
+          setModal({
+            show: true,
+            msg: 'Error '+error+' Contact carloscruz85@gmail.com '
+        })
+        })
+    }, [])
 
     return (
         <>
             { modal.show ? <Modal msg={modal.msg}/> : null }
-            In home
+            <div className="machine-container">
+                <div className="machine">
+                    <div className="title">
+                        Cruz Machine
+                    </div>
+                    <div className="products">
+                        
+                    </div>
+                    <div className="screen">
+                        <div className="waiter">
+                            
+                        </div>
+                    </div>
+
+                </div>
+            </div>
         </>
     )
 }

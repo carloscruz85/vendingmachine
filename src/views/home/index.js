fd2dfee2691483fad22a8fe773200a8c03ccd44d
carloscruz85 <carloscruz85@gmail.com>
2021-08-09 13:44:25 -0600

    Getting API response

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import axios from 'axios'
 
 const Home = () => {
+    //getting data from API https://vending-machine-test.vercel.app/api/products
+    useEffect(() => {
+        axios.get('https://vending-machine-test.vercel.app/api/products')
+        .then(res => {
+            console.log(res);
+        })
+        .catch(error => {
+          console.log('Error', error)
+        })
+    }, [])
+
     return (
         <div>
-            Home
+            In home
         </div>
     )
 }
